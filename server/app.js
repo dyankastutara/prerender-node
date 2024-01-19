31646c353e95a8c4e81da610fa3c8493a842cce7
Dyan Kastutara <dyankastutara19@gmail.com>
2024-01-19 18:10:39 +0700

    add use prerender

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const prerender= require('prerender-node');
 
 const app = express();
+
+app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN))
 
 app.listen(3001, () => {
    console.log("App run on Port 3001")
