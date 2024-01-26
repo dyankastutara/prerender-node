852e98d2e8c8f2c5378d83c56710803e72134bcb
Dyan Kastutara <dyankastutara19@gmail.com>
2024-01-26 15:54:40 +0700

    remove prerender

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const prerender= require('prerender-node');
 const routes = require('./routes');
 const app = express();
 
 app.use('/provinces', routes.provinces);
-
-app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN))
 
 app.listen(3001, () => {
    console.log("App run on Port 3001");
